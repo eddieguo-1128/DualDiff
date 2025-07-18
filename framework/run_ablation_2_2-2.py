4acfd3b4a892fa9198c68decf3798d5926b5d127
Leviathan370 <zhangmengchun37@163.com>
2025-07-18 10:34:58 -0400

    kara-one

--- framework/run_ablation_2_2-2.py
+++ framework/run_ablation_2_2-2.py
@@ -9,7 +9,7 @@ from config import work_dir, use_subject_wise_z_norm
 seeds = [42, 43, 44]
 ddpm_variants = ["use_ddpm", "no_ddpm"] # no ddpm means no x_hat is generated
 encoder_inputs = ["x", "x_hat"] # x_hat is only available when ddpm is used
-decoder_inputs = ["z only"] # "z only" is the default
+decoder_inputs = ["z + x_hat", "z + skips"] # "z only" is the default
 decoder_variants = ["use_decoder", "no_decoder"] # no decoder means no decoder_out is generated
 z_local_norm_mode = "option2" # option1: directly claculate z_statistics across sessions; option2: calculate z_statistics by sessions and then average 
 z_norm_mode = "option2" 
