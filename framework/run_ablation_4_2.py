ce0c1cbf665bdd7b2050f3d5cde516ab21adb139
Leviathan370 <zhangmengchun37@163.com>
2025-07-19 14:22:35 -0400

    kara-one

--- framework/run_ablation_4_2.py
+++ framework/run_ablation_4_2.py
@@ -6,15 +6,15 @@ from datetime import datetime
 from config import work_dir, use_subject_wise_z_norm
 
 # Define ablation axes
-seeds = [42]
+seeds = [44]
 ddpm_variants = ["use_ddpm", "no_ddpm"] # no ddpm means no x_hat is generated
 encoder_inputs = ["x", "x_hat"] # x_hat is only available when ddpm is used
 decoder_inputs = ["z only"]
 decoder_variants = ["use_decoder", "no_decoder"] # no decoder means no decoder_out is generated 
-z_local_norm_mode = "option2" # option1: directly claculate z_statistics across sessions; option2: calculate z_statistics by sessions and then average 
 z_norm_mode = ["option1", "option2"] # "option2" is the default
+z_local_norm_mode = "option2" # option1: directly claculate z_statistics across sessions; option2: calculate z_statistics by sessions and then average 
 classifier_variants = ["fc_classifier"] # "fc_classifier" is default
-classifier_inputs = ["x"] # "z" is the default 
+classifier_inputs = ["decoder_out"] # "z" is the default 
 mixup_strategy = ["none", "inputs weighted average", "inputs temporal mixup", 
                   "prior embeddings weighted average", "later embeddings weighted average"] # ablate later
 ddpm_loss = [True, False] # ablate later 
