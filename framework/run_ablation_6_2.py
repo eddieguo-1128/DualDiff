80394f1c29a9d4be1fa47b9b63e16d7aeb8321f3
Leviathan370 <zhangmengchun37@163.com>
2025-07-21 11:22:58 -0400

    kara-one

--- framework/run_ablation_6_2.py
+++ framework/run_ablation_6_2.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from config import work_dir, use_subject_wise_z_norm
 
 # Define ablation axes
-seeds = [44]
+seeds = [42, 43, 44]
 ddpm_variants = ["use_ddpm", "no_ddpm"] # no ddpm means no x_hat is generated
 encoder_inputs = ["x", "x_hat"] # x_hat is only available when ddpm is used
 decoder_inputs = ["x + x_hat + skips", "x + x_hat", "x_hat + skips", "x + skips",
@@ -24,7 +24,7 @@ decoder_reconstruction_loss = "L1" # default is L1
 
 alphas = [1] # default is 1
 betas = ["scheduler to 0.05"] # default is "scheduler to 0.05"
-gammas = [0] # default is "scheduler to 0.2"
+gammas = ["scheduler to 0.2"] # default is "scheduler to 0.2"
 
 mixup_strategy = ["none", "inputs weighted average", "inputs temporal mixup", 
                   "prior embeddings weighted average", "later embeddings weighted average"] # ablate later
