5bddaf176a4d2840e8a0c725142bf8495ff4e719
Leviathan370 <187221291+MengchunZhang37@users.noreply.github.com>
2025-09-24 02:06:33 -0400

    baseline

--- framework/run_ablation_8_3.py
+++ framework/run_ablation_8_3.py
@@ -4,7 +4,7 @@ import pandas as pd
 import subprocess
 from datetime import datetime
 # from framework.config import work_dir
-from framework.config1 import work_dir, use_subject_wise_z_norm
+from config import work_dir, use_subject_wise_z_norm
 
 # ------------ Settings -------------
 seeds = [42,43,44]  
